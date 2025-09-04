af8b7c89e9ec9a240314a98435d845655a5dc9cf
an133444 <adsantana@ucundinamarca.edu.co>
2025-09-04 10:46:07 -0500

    creamos la memoria estatica (o inmutable )

---
+++ SRC/M-SDRDC.py
@@ -0,0 +1,13 @@
+estudent = input("ingresa el nombre de el estudiante: ")
+
+cursos = (
+    "calculo multivariado",
+    "lengua extranjera",
+    "fisca III",
+    "ingles II",
+    "estructuras de informacion",
+    "arqutectura de computadoras"
+
+)
+
+nota_maxima = 5.0
